64f9dac874a7168b8d8525bdcf25259de5d08c25
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2024-11-28 20:11:55 +0530

    Time: 463 ms (89.61%), Space: 12.3 MB (93.77%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -6,18 +6,18 @@ class Solution(object):
         :rtype: int
         """
         n, m = len(text1), len(text2)
-        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        prev = [0 for _ in range(m + 1)]
+        curr = [0 for _ in range(m + 1)]
 
         for j in range(m):
-            dp[0][j] = 0
-        for i in range(n):
-            dp[i][0] = 0
+            prev[j] = 0
 
         for i in range(1, n + 1):
             for j in range(1, m + 1):
                 if text1[i -1] == text2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                    curr[j] = 1 + prev[j - 1]
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev = curr[:]
 
-        return dp[n][m]
+        return prev[m]

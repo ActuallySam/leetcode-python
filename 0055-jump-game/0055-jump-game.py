cc0de00ee27411772f46557b4059a2f4c863cf10
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2024-12-02 02:24:45 +0530

    Time: 36 ms (30.53%), Space: 13.1 MB (7.01%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        maxIndex = 0
+        for i, num in enumerate(nums):
+            if i > maxIndex:
+                return False
+            maxIndex = max(maxIndex, i + num)
+
+        return True

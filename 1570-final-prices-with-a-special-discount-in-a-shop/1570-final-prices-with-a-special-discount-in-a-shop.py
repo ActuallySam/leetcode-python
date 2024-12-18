d02d29841da7c38518bd50bc01f358c1216c8b29
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2024-12-19 04:18:24 +0530

    Time: 3 ms (75.82%), Space: 12.3 MB (8.79%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -5,21 +5,30 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(prices)
-        res = [None] * n
-        res[-1] = prices[-1]
+        # res = [None] * n
+        # res[-1] = prices[-1]
 
-        for i in range(n):
-            j = i + 1
-            price = prices[i]
-            while j < n:
-                print(i, prices[j] > prices[i])
-                if j == n - 1 and prices[j] > prices[i]:
-                    res[i] = prices[i]
-                elif prices[j] <= prices[i]:
-                    discount = price - prices[j]
-                    res[i] = discount
-                    break
+        # for i in range(n):
+        #     j = i + 1
+        #     price = prices[i]
+        #     while j < n:
+        #         print(i, prices[j] > prices[i])
+        #         if j == n - 1 and prices[j] > prices[i]:
+        #             res[i] = prices[i]
+        #         elif prices[j] <= prices[i]:
+        #             discount = price - prices[j]
+        #             res[i] = discount
+        #             break
 
-                j += 1
+        #         j += 1
         
-        return res
+        # return res
+
+        stack = []
+        for i in range(n):
+            while stack and prices[i] <= prices[stack[-1]]:
+                prices[stack[-1]] = prices[stack[-1]] - prices[i]
+                stack.pop()
+            stack.append(i)
+        return prices
+

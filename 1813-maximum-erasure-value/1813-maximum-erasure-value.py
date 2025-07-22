1ee382e33ce26072ca3966650957c554991f3d85
Samarth Srivastava <63175337+ActuallySam@users.noreply.github.com>
2025-07-23 04:45:37 +0530

    Time: 7025 ms (5%), Space: 28.9 MB (30.09%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.py
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -7,16 +7,17 @@ class Solution:
         right = 0
         curr_sum = 0
         while right < n:
-            if nums[right] in vis:
+            num_right = nums[right]
+            if num_right in vis:
                 max_score = max(max_score, sum(vis))
-                while nums[right] in vis:
+                while num_right in vis:
                     curr_sum -= nums[left]
                     vis.remove(nums[left])
                     left += 1
 
-            curr_sum += nums[right]
-            vis.add(nums[right])
+            curr_sum += num_right
+            vis.add(num_right)
             right += 1
             
-        return max(max_score, sum(vis))
+        return max(max_score, curr_sum)
         
